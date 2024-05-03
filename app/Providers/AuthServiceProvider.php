d69589a36a26e22808e8a52632006f6cda7ee92d
panjialbir <115150326+panjialbir@users.noreply.github.com>
2024-05-03 15:10:10 +0700

    middleware, gate admin

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -21,7 +21,9 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->registerPolicies();
+        Gate::define('admin', function(User $user){
+            return $user->is_admin == true;
+        });
 
         //
     }
